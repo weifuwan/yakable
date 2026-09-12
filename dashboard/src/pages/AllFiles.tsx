7c2acdf52d7f68bc8dfa3fcc12998bd6a5a7b999
weifuwan <295227940@qq.com>
2026-09-12 12:17:45 +0800

    updategg

---
+++ dashboard/src/pages/AllFiles.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "./PlaceholderPage";
+
+export function AllFiles() {
+  return <PlaceholderPage title="Files" description="Project files and uploaded assets will appear here." />;
+}
