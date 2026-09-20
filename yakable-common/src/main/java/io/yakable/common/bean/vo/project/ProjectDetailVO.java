0b8325b670e4b0fd09de4bf092c878edd120bd23
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:28:57 +0800

    refactor(common): group dto vo enum by domain

---
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/project/ProjectDetailVO.java
@@ -0,0 +1,21 @@
+package io.yakable.common.bean.vo.project;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Project 详情页面返回对象。
+ */
+@Getter
+@Setter
+public class ProjectDetailVO {
+
+    private String id;
+    private String name;
+    private String latestSessionId;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
