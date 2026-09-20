7a246c4fc87e51d40402d9a484bc5c47f04c0e93
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 12:36:49 +0800

    refactor(dao): keep project paging inside ProjectRepository

--- yakable-dao/src/main/java/io/yakable/dao/repository/ProjectRepository.java
+++ yakable-dao/src/main/java/io/yakable/dao/repository/ProjectRepository.java
@@ -1,9 +1,16 @@
 package io.yakable.dao.repository;
 
+import io.yakable.common.bean.PageData;
+import io.yakable.common.bean.dto.common.PageDTO;
 import io.yakable.dao.entity.ProjectEntity;
 
 /**
  * Project 数据访问入口。
  */
 public interface ProjectRepository extends BaseRepository<ProjectEntity> {
+
+    /**
+     * 按更新时间倒序分页查询 Project。
+     */
+    PageData<ProjectEntity> queryProject(PageDTO dto);
 }
