0eaeb1ed960b1abd289d8ba954f722c092e65fcd
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:29:55 +0800

    refactor(common): group session types by domain

---
+++ yakable-common/src/main/java/io/yakable/common/enums/session/MessageRoleEnum.java
@@ -0,0 +1,20 @@
+package io.yakable.common.enums.session;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Message 角色。
+ */
+@Getter
+@AllArgsConstructor
+public enum MessageRoleEnum {
+
+    USER(0),
+    ASSISTANT(1),
+    SYSTEM(2);
+
+    @EnumValue
+    private final Integer value;
+}
