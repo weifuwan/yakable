504822b11b1947790908dacdab57309bb91237c8
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 14:17:56 +0800

    docs(common): document TurnInvocationVO schema

--- yakable-common/src/main/java/io/yakable/common/bean/vo/session/TurnInvocationVO.java
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/session/TurnInvocationVO.java
@@ -1,5 +1,6 @@
 package io.yakable.common.bean.vo.session;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +9,21 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "Turn 模型调用信息")
 public class TurnInvocationVO {
 
+    @Schema(description = "模型提供商")
     private String provider;
+
+    @Schema(description = "模型名称")
     private String model;
+
+    @Schema(description = "Token 使用信息")
     private TokenUsageVO usage;
+
+    @Schema(description = "模型提供商请求 ID")
     private String providerRequestId;
+
+    @Schema(description = "模型调用结束原因")
     private String finishReason;
 }
