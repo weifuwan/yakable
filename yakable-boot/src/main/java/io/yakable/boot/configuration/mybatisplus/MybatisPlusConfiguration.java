2fa573bc1c52bf68aadc7f061c5707eef05e19c8
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 13:51:27 +0800

    refactor(boot): complete MyBatis-Plus configuration

--- yakable-boot/src/main/java/io/yakable/boot/configuration/mybatisplus/MybatisPlusConfiguration.java
+++ yakable-boot/src/main/java/io/yakable/boot/configuration/mybatisplus/MybatisPlusConfiguration.java
@@ -1,8 +1,13 @@
 package io.yakable.boot.configuration.mybatisplus;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +19,34 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan("io.yakable.dao.mapper")
 public class MybatisPlusConfiguration {
 
+    private static final long MAX_PAGE_SIZE = 500L;
+
     @Bean
     MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(paginationInterceptor());
         return interceptor;
+    }
+
+    @Bean
+    MybatisPlusPropertiesCustomizer mybatisPlusPropertiesCustomizer() {
+        return properties -> {
+            MybatisConfiguration configuration = new MybatisConfiguration();
+            configuration.setJdbcTypeForNull(JdbcType.NULL);
+            configuration.setMapUnderscoreToCamelCase(true);
+            properties.setConfiguration(configuration);
+            properties.setMapperLocations(new String[]{"classpath*:/mapper/**/*.xml"});
+
+            GlobalConfig globalConfig = GlobalConfigUtils.getGlobalConfig(configuration);
+            globalConfig.setBanner(false);
+            properties.setGlobalConfig(globalConfig);
+        };
+    }
+
+    private PaginationInnerInterceptor paginationInterceptor() {
+        PaginationInnerInterceptor pagination = new PaginationInnerInterceptor(DbType.MYSQL);
+        pagination.setOverflow(false);
+        pagination.setMaxLimit(MAX_PAGE_SIZE);
+        return pagination;
     }
 }
