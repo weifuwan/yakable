45f57e7a2cc2f6b88113ef46afe4dcc3f6a785bc
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 14:17:01 +0800

    docs(common): document MessageVO schema

--- yakable-common/src/main/java/io/yakable/common/bean/vo/session/MessageVO.java
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/session/MessageVO.java
@@ -1,5 +1,6 @@
 package io.yakable.common.bean.vo.session;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +11,24 @@ import java.time.LocalDateTime;
  */
 @Getter
 @Setter
+@Schema(description = "Session 消息")
 public class MessageVO {
 
+    @Schema(description = "Message ID")
     private String id;
+
+    @Schema(description = "所属 Turn ID")
     private String turnId;
+
+    @Schema(description = "消息角色")
     private String role;
+
+    @Schema(description = "消息内容")
     private String content;
+
+    @Schema(description = "消息序号")
     private Long sequence;
+
+    @Schema(description = "创建时间")
     private LocalDateTime createdAt;
 }
