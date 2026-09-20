618bded49aaa1900fb29e2ce3115bd94faf4475b
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:28:55 +0800

    refactor(common): group dto vo enum by domain

---
+++ yakable-common/src/main/java/io/yakable/common/bean/dto/project/QueryProjectDTO.java
@@ -0,0 +1,10 @@
+package io.yakable.common.bean.dto.project;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 查询单个 Project 入参。
+ *
+ * @param projectId Project ID
+ */
+public record QueryProjectDTO(@NotBlank String projectId) {}
