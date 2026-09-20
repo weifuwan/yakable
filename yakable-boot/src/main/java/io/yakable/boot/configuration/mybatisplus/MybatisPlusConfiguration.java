df7383bbf507693d32fe086dc270efd8b8f825ac
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 13:19:15 +0800

    refactor(boot): move MyBatis-Plus configuration to boot

---
+++ yakable-boot/src/main/java/io/yakable/boot/configuration/mybatisplus/MybatisPlusConfiguration.java
@@ -0,0 +1,23 @@
+package io.yakable.boot.configuration.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis-Plus 配置。
+ */
+@Configuration(proxyBeanMethods = false)
+@MapperScan("io.yakable.dao.mapper")
+public class MybatisPlusConfiguration {
+
+    @Bean
+    MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
