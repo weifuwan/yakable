252b10fe24c86a4b446f76ceb43c0b22c9556ecf
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 14:17:48 +0800

    docs(common): document TokenUsageVO schema

--- yakable-common/src/main/java/io/yakable/common/bean/vo/session/TokenUsageVO.java
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/session/TokenUsageVO.java
@@ -1,5 +1,6 @@
 package io.yakable.common.bean.vo.session;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,15 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "Token 使用信息")
 public class TokenUsageVO {
 
+    @Schema(description = "输入 Token 数")
     private Long inputTokens;
+
+    @Schema(description = "输出 Token 数")
     private Long outputTokens;
+
+    @Schema(description = "总 Token 数")
     private Long totalTokens;
 }
