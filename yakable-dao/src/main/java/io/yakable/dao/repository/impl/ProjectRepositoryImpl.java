6aa5b3409ee3780b29f1adee88e897be87aadec0
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 10:36:37 +0800

    refactor(dao): add ProjectRepositoryImpl

---
+++ yakable-dao/src/main/java/io/yakable/dao/repository/impl/ProjectRepositoryImpl.java
@@ -0,0 +1,50 @@
+package io.yakable.dao.repository.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.yakable.common.bean.PageData;
+import io.yakable.common.bean.dto.PageDTO;
+import io.yakable.dao.entity.ProjectEntity;
+import io.yakable.dao.mapper.ProjectMapper;
+import io.yakable.dao.repository.BaseRepository;
+import io.yakable.dao.repository.ProjectRepository;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@DependsOn("yakableFlyway")
+public class ProjectRepositoryImpl extends BaseRepository<ProjectMapper, ProjectEntity> implements ProjectRepository {
+
+    @Resource
+    private ProjectMapper projectMapper;
+
+    @Override
+    protected ProjectMapper mapper() {
+        return projectMapper;
+    }
+
+    @Override
+    public ProjectEntity addProject(ProjectEntity entity) {
+        return add(entity);
+    }
+
+    @Override
+    public PageData<ProjectEntity> queryProject(PageDTO dto) {
+        Page<ProjectEntity> page = new Page<>(dto.getCurrent(), dto.getPageSize());
+        IPage<ProjectEntity> result = projectMapper.selectProjectPage(page);
+        return new PageData<>(
+                result.getRecords(),
+                result.getTotal(),
+                result.getPages(),
+                Math.toIntExact(result.getCurrent()),
+                Math.toIntExact(result.getSize()));
+    }
+
+    @Override
+    public Optional<ProjectEntity> queryProject(String projectId) {
+        return Optional.ofNullable(projectMapper.selectProjectDetails(projectId));
+    }
+}
