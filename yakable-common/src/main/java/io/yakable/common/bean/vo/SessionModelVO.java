ae3aed4af1ee0fbb319ff1ae168bf9d799d3e7de
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:09:00 +0800

    refactor(common): add SessionModelVO

---
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/SessionModelVO.java
@@ -0,0 +1,15 @@
+package io.yakable.common.bean.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Session 模型信息。
+ */
+@Getter
+@Setter
+public class SessionModelVO {
+
+    private String provider;
+    private String model;
+}
