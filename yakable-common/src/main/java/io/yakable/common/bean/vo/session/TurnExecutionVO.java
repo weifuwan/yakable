782c8bdb8c6bbee991bfb99b62ba38536bee3fa6
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 12:32:49 +0800

    refactor(common): add TurnExecutionVO

---
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/session/TurnExecutionVO.java
@@ -0,0 +1,15 @@
+package io.yakable.common.bean.vo.session;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Turn 执行上下文。
+ */
+@Getter
+@Setter
+public class TurnExecutionVO {
+
+    private String id;
+    private String sessionId;
+}
