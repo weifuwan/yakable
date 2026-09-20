2263dcbeaf388377160a54899c653eeccdaa22d7
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 09:32:47 +0800

    refactor(common): move project query dto to common

---
+++ yakable-common/src/main/java/io/yakable/common/bean/dto/QueryProjectDTO.java
@@ -0,0 +1,13 @@
+package io.yakable.common.bean.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 查询单个 Project 入参。
+ *
+ * @param projectId Project ID
+ */
+public record QueryProjectDTO(
+        @NotBlank String projectId
+) {
+}
