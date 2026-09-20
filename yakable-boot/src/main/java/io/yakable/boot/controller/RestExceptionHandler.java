572e73aa06a197e578acd50949a57c401573019f
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:19:35 +0800

    refactor(boot): handle SessionException by ErrorCode

--- yakable-boot/src/main/java/io/yakable/boot/controller/RestExceptionHandler.java
+++ yakable-boot/src/main/java/io/yakable/boot/controller/RestExceptionHandler.java
@@ -1,8 +1,7 @@
 package io.yakable.boot.controller;
 
-import io.yakable.common.exception.SessionBusyException;
-import io.yakable.common.exception.SessionInactiveException;
-import io.yakable.common.exception.SessionNotFoundException;
+import io.yakable.common.exception.SessionException;
+import io.yakable.service.session.SessionErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,14 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(SessionNotFoundException.class)
-    ProblemDetail handleNotFound(SessionNotFoundException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
-    }
-
-    @ExceptionHandler({SessionBusyException.class, SessionInactiveException.class})
-    ProblemDetail handleConflict(RuntimeException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, exception.getMessage());
+    @ExceptionHandler(SessionException.class)
+    ProblemDetail handleSessionException(SessionException exception) {
+        HttpStatus status = exception.getErrorCode() == SessionErrorCode.NOT_FOUND
+                ? HttpStatus.NOT_FOUND
+                : HttpStatus.CONFLICT;
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(status, exception.getErrorCode().getMessage());
+        detail.setProperty("code", exception.getErrorCode().getCode());
+        return detail;
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
