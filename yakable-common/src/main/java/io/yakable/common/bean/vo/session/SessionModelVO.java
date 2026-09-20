358e2f3475d390cb4962b5f1704437b506f3eb8f
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 11:29:41 +0800

    refactor(common): group session types by domain

---
+++ yakable-common/src/main/java/io/yakable/common/bean/vo/session/SessionModelVO.java
@@ -0,0 +1,15 @@
+package io.yakable.common.bean.vo.session;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Session 模型信息。
+ */
+@Getter
+@Setter
+public class SessionModelVO {
+
+    private String provider;
+    private String model;
+}
