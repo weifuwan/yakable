cc9e97cedcabb13d7e58a8c36cf64a1670ba2c0e
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 10:16:16 +0800

    feat(dao): enable mybatis-plus pagination

--- yakable-dao/src/main/java/io/yakable/dao/config/DaoConfiguration.java
+++ yakable-dao/src/main/java/io/yakable/dao/config/DaoConfiguration.java
@@ -1,5 +1,8 @@
 package io.yakable.dao.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.flywaydb.core.Flyway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,13 @@ import javax.sql.DataSource;
 @Configuration(proxyBeanMethods = false)
 @MapperScan("io.yakable.dao.mapper")
 public class DaoConfiguration {
+
+    @Bean
+    MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 
     @Bean(name = "yakableFlyway", initMethod = "migrate")
     Flyway yakableFlyway(DataSource dataSource) {
