21bb68ab6caaf23f722f848761d19704edbedb6e
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-20 10:06:41 +0800

    feat(common): add system constants

---
+++ yakable-common/src/main/java/io/yakable/common/constant/SystemConstant.java
@@ -0,0 +1,12 @@
+package io.yakable.common.constant;
+
+/**
+ * 系统公共常量。
+ */
+public final class SystemConstant {
+
+    public static final String SYSTEM_USER = "system";
+
+    private SystemConstant() {
+    }
+}
