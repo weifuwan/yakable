0f3ce5abd7b3db304daacfcd66d720537415eb52
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-22 18:34:52 +0800

    feat: add lightweight navigation message mapper

--- yakable-dao/src/main/java/io/yakable/dao/mapper/MessageMapper.java
+++ yakable-dao/src/main/java/io/yakable/dao/mapper/MessageMapper.java
@@ -2,6 +2,11 @@ package io.yakable.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import io.yakable.dao.entity.MessageEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MessageMapper extends BaseMapper<MessageEntity> {
+
+    List<MessageEntity> selectUserNavigationMessageList(@Param("sessionId") String sessionId);
 }
