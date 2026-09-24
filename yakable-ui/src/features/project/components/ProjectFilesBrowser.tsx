f6162216e8a26f7e8725ed3e17249557ac880546
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 10:36:10 +0800

    style: apply project files browser formatting

--- yakable-ui/src/features/project/components/ProjectFilesBrowser.tsx
+++ yakable-ui/src/features/project/components/ProjectFilesBrowser.tsx
@@ -102,11 +102,7 @@ function ProjectFilesBrowserContent({ projectId }: ProjectFilesBrowserProps) {
       ) : (
         <div className="flex min-h-0 flex-1">
           <div className="flex min-h-0 w-52 shrink-0 flex-col border-r border-border">
-            <ProjectFilesTree
-              files={files}
-              selectedPath={selectedPath}
-              onSelect={selectFile}
-            />
+            <ProjectFilesTree files={files} selectedPath={selectedPath} onSelect={selectFile} />
           </div>
 
           <div className="flex min-w-0 flex-1 flex-col">
