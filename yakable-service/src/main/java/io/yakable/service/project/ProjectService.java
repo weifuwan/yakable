090fef4bae47bb0a3c409b112ad99695d7b497f0
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 10:05:06 +0800

    feat: expose project files service API

--- yakable-service/src/main/java/io/yakable/service/project/ProjectService.java
+++ yakable-service/src/main/java/io/yakable/service/project/ProjectService.java
@@ -2,7 +2,11 @@ package io.yakable.service.project;
 
 import io.yakable.common.bean.PageData;
 import io.yakable.common.bean.dto.project.AddProjectDTO;
+import io.yakable.common.bean.dto.project.QueryProjectFileDTO;
+import io.yakable.common.bean.dto.project.QueryProjectFilesDTO;
 import io.yakable.common.bean.dto.project.QueryProjectPageDTO;
+import io.yakable.common.bean.vo.project.ProjectFileVO;
+import io.yakable.common.bean.vo.project.ProjectFilesVO;
 import io.yakable.common.bean.vo.project.ProjectListVO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -21,4 +25,14 @@ public interface ProjectService {
      * 分页查询当前用户 Project。
      */
     PageData<ProjectListVO> queryProject(@NotNull @Valid QueryProjectPageDTO dto);
+
+    /**
+     * 查询当前用户 Project 已发布文件列表。
+     */
+    ProjectFilesVO queryProjectFiles(@NotNull @Valid QueryProjectFilesDTO dto);
+
+    /**
+     * 查询当前用户 Project 单个已发布文件。
+     */
+    ProjectFileVO queryProjectFile(@NotNull @Valid QueryProjectFileDTO dto);
 }
